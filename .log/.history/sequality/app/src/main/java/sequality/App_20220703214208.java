b7babfd6250140868cc5b086eea9d3ad2bfba4e7
shirai000 <m1m22a19@st.oit.ac.jp>
2022-07-03 21:46:58 +0900

    add gradle project

--- .log/.history/sequality/app/src/main/java/sequality/App_20220703214208.java
+++ .log/.history/sequality/app/src/main/java/sequality/App_20220703214208.java
@@ -30,26 +30,11 @@ public class App {
     return sum;
   }
 
-  public int sumE(int x, int y) {
-    int sum = 0;
-    for (int i = 0; i < y - x + 1; i++) {
-      if ((x + i) % 2 == 0) {
-        sum = sum + (x + i);
-      }
-    }
-    return sum;
-  }
-
   public static void main(String[] args) {
     System.out.println(new App().getGreeting());
     System.out
-        .println(
-            "Sum of 2 and 3 is " + new App().sum(2, 3) + " Average is " + (double) (new App().sum(2, 3)) / 2 + ".");
+        .println("Sum of 2 and 3 is" + new App().sum(2, 3) + " Average is " + (double) (new App().sum(2, 3)) / 2 + ".");
     System.out
-        .println("Sum of 1 to 10 is " + new App().sumA(1, 10) + " Average is " + (double) (new App().sumA(1, 10)) / 10
-            + ".");
-    System.out
-        .println(
-            "Sum of odd of 1 to 10 is " + new App().sumO(1, 10) + " Sum of even is " + new App().sumE(1, 10) + ".");
+        .println("Sum of 2 and 3 is" + new App().sumA(1, 10) + " Average is " + (double) (new App().sumA(1, 10)) / 10 + ".");
   }
 }
